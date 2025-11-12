50768faa3b3443fd453a8798016b41945d2b12cc
Patrick Otero <patrick.rocha.otero@gmail.com>
2025-11-12 20:09:30 -0300

    Changed the client id to the correct one

--- src/components/organisms/AccountPage/ScheduleTab/index.tsx
+++ src/components/organisms/AccountPage/ScheduleTab/index.tsx
@@ -128,7 +128,7 @@ export function ScheduleTab() {
   };
 
   const startOAuthCalendlySync = () => {
-    const calendlyClientId = '0FCoWFaytwSPcPUI2FSxLAxmGHNfLaXrye7in6WXkmY';
+    const calendlyClientId = 'Vx2DRKhKAvTcl5y8N1SqGg0OQ-9HR4KTO62t29C5L8M';
     const redirectUri =
       'https://p01--mentores-backend-api-dev--bj8pjy8s82zl.code.run/calendly/callback';
     const calendlyAuthUrl = `https://auth.calendly.com/oauth/authorize?client_id=${calendlyClientId}&response_type=code&redirect_uri=${redirectUri}&state=${encodeURIComponent(String(mentor.data?.id))}`;
